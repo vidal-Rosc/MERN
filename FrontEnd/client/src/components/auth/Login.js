4bc49f393807436a8dc0bf8273069b1a2b5df769
vidal-Rosc <vidal.rocca86@gmail.com>
2021-03-08 13:49:55 +0100

    creando el login

--- FrontEnd/client/src/components/auth/Login.js
+++ FrontEnd/client/src/components/auth/Login.js
@@ -3,8 +3,49 @@ import React from 'react';
 
 
 const Login = () => {
+
+    const onChange = () => {
+
+    }
+
+
     return ( 
-        <h1> Desde Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>*** Login ***</h1>
+
+                <form>
+                    <div className="campo-form">
+                        <label htmlFor="email"> Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Enter your email..."
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <label htmlFor="password"> Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <input
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Login"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
      );
 }
  
