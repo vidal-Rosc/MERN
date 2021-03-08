dbfcbcbcce6f9335e4b211ea00af5ef10c12e7c5
vidal-Rosc <vidal.rocca86@gmail.com>
2021-03-08 13:27:47 +0100

    Agregando el Routing de los components

--- FrontEnd/client/src/App.js
+++ FrontEnd/client/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Switch, Route } from  'react-router-dom';
+import Login from './components/auth/Login';
+import NewAccount from './components/auth/NewAccount';
+import Projects from './components/projects/Projects';
 
 function App() {
+  //Todo lo que va dentro el Switch es el contenido de CADA UNA de las paginas
+  //Todo lo que va fuera del switch se vera EN TODAS las paginas
   return (
-    <div className="App">
-      <h1>MERN frontend</h1>
-    </div>
+    <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/new-account" component={NewAccount} />
+          <Route exact path="/projects" component={Projects} />
+        </Switch>
+    </Router>
   );
 }
 
