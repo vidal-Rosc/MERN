f51f54ca4189796b351896014275b438957ee9a5
vidal-Rosc <vidal.rocca86@gmail.com>
2021-03-09 22:10:09 +0100

    creating the component bar

---
+++ FrontEnd/client/src/components/layout/Bar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Bar = () => {
+    return (  
+        <header className="app-header">
+            <p className="user-name"> welcome back, <span>Vidal</span></p>
+
+            <nav className="nav-principal">
+                <a href="#!">Sign Out</a>
+            </nav>
+
+        </header>
+    );
+}
+ 
+export default Bar;
