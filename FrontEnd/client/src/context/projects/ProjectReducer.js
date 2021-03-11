aea77a3efad5975bb43f56578a104a2345cc50da
vidal-Rosc <vidal.rocca86@gmail.com>
2021-03-11 16:46:17 +0100

    nuevos cases

--- FrontEnd/client/src/context/projects/ProjectReducer.js
+++ FrontEnd/client/src/context/projects/ProjectReducer.js
@@ -1,4 +1,4 @@
-import { FORM_PROJECT, GET_PROJECTS, ADD_PROJECT, VALIDATE_FORM } from '../../types';
+import { FORM_PROJECT, GET_PROJECTS, ADD_PROJECT, VALIDATE_FORM, ACTUAL_PROJECT, DELETE_PROJECT } from '../../types';
 
 
 //Contiene las funciones que van a interactuar con el state
@@ -28,7 +28,19 @@ export default (state, action) => {
             return {
                 ...state,
                 errorForm: true
-  
+            }
+        case ACTUAL_PROJECT:
+            return {
+                ...state,
+                project: state.proyectos.filter(project => project.id === action.payload)
+
+            }
+        case DELETE_PROJECT:
+            return {
+                ...state,
+                proyectos: state.proyectos.filter(project => project.id !== action.payload),
+                //Para que al borrar elimine tb el formulario de tareas
+                project: null
             }
         default:
             return state;
