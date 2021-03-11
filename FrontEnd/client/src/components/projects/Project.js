c5f6d04c52b2e6132147ce055a64608c0c88b1d5
vidal-Rosc <vidal.rocca86@gmail.com>
2021-03-11 16:47:10 +0100

    mostrando el projecto actual

--- FrontEnd/client/src/components/projects/Project.js
+++ FrontEnd/client/src/components/projects/Project.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ProjectContext from '../../context/projects/ProjectContext';
 
 
 const Project = ({project}) => {
+    //State para los proyectos
+    const projectsContext = useContext(ProjectContext);
+    const {actualProject } = projectsContext;
+
+
     return ( 
         <li>
             <button
                 type="button"
                 className="btn btn-blank"
+                onClick={() => actualProject(project.id)}
             >{project.name}</button>
         </li>
     );
