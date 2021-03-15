daa79e11198adb98d8b4b327bfca2290aaa55361
vidal-Rosc <vidal.rocca86@gmail.com>
2021-03-15 10:21:39 +0100

    cambiando el status de las tareas

--- FrontEnd/client/src/components/tasks/Task.js
+++ FrontEnd/client/src/components/tasks/Task.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ProjectContext from '../../context/projects/ProjectContext';
+import TaskContext from '../../context/tasks/taskContext';
 
 
 
 const Task = ({task}) => {
+
+    //Para obtener el proyecto actual
+    const projectsContext = useContext(ProjectContext);
+    const { project } = projectsContext;
+
+    //Obtenemos la funcion para eliminar las tareas
+    const tasksContext = useContext(TaskContext);
+    const { deleteTask, getTasks, taskStatus } = tasksContext;
+
+    const [actualProject] = project;
+
+    //Al eliminar una tarea
+    const delTask = id => {
+        deleteTask(id);
+        getTasks(actualProject.id);
+    }
+
+    //Para modificar el status de las tareas 
+    const changeTaskStatus = task => {
+    //if(task.status){
+    //    task.status= false;
+    //}else{
+    //   task.status= true; 
+    //}
+    //taskStatus(task);
+        task.status ? task.status= false : task.status = true;
+        taskStatus(task)
+    }
+
+    
+
     return ( 
         <li className="task shadow">
             <p>{task.name}</p>
@@ -13,12 +46,14 @@ const Task = ({task}) => {
                     <button
                         type="button"
                         className="completed"
+                        onClick={() => changeTaskStatus(task)}
                     >Done!</button>
                 )
                 :(
                     <button
                         type="button"
                         className="incompleted"
+                        onClick={() => changeTaskStatus(task)}
                     >Not Yet!</button>
                 )
                 }
@@ -34,6 +69,7 @@ const Task = ({task}) => {
                 <button
                     type="button"
                     className="btn btn-secondary"
+                    onClick={() => delTask(task.id)}
                 >Delete</button>
 
             </div>
