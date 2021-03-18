89d5d94960bc907239d3e5918697812a372d164c
vidal-Rosc <vidal.rocca86@gmail.com>
2021-03-18 17:22:25 +0100

    borrando un proyecto

--- BackEnd/servidor/routes/projects.js
+++ BackEnd/servidor/routes/projects.js
@@ -12,16 +12,43 @@ const  projectController = require('../controllers/projectController');
 //Importamos nuestro middleware
 const auth = require('../middlewares/auth');
 
+//Para validar
+const { check } = require('express-validator');
+
+
+
+
 //Creamos un nuevo proyecto
 // /api/projects
 router.post('/',
     auth,
+    [
+        check('name', 'The project name is required').not().isEmpty()
+    ],
     projectController.createProject
 );
 
+//Obtenemos todos los proyectos
 // /api/projects
 router.get('/',
     auth,
-    projectController.createProject
+    projectController.getProjects
+);
+
+//Actualizamos un proyecto por id
+// /api/projects
+router.put('/:id',
+    auth,
+    [
+        check('name', 'The project name is required').not().isEmpty()
+    ],
+    projectController.updateProject
+)
+
+//Borramos un proyecto por id
+// /api/projects
+router.delete('/:id',
+    auth,
+    projectController.deleteProject
 )
 module.exports = router;
