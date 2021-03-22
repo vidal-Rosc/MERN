3e832e60b2283c215993ad72c9094d659fbf2ce3
vidal-Rosc <vidal.rocca86@gmail.com>
2021-03-22 09:23:18 +0100

    types de alertas

--- FrontEnd/client/src/types/index.js
+++ FrontEnd/client/src/types/index.js
@@ -13,3 +13,6 @@ export const TASK_STATUS = 'TASK_STATUS';
 export const ACTUAL_TASK = 'ACTUAL_TASK';
 export const UPDATE_TASK = 'UPDATE_TASK';
 export const CLEAN_TASK = 'CLEAN_TASK';
+
+export const DISPLAY_ALERT = 'DISPLAY_ALERT';
+export const HIDE_ALERT = 'HIDE_ALERT';
