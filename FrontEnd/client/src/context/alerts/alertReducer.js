99c0573e92a1acd2d0fbebb2c632ee0570308cce
vidal-Rosc <vidal.rocca86@gmail.com>
2021-03-22 09:21:52 +0100

    reducer para los mensajes de alerta

---
+++ FrontEnd/client/src/context/alerts/alertReducer.js
@@ -0,0 +1,20 @@
+import {DISPLAY_ALERT, HIDE_ALERT} from '../../types/';
+
+
+
+export default (state, action) => {
+    switch(action.type){
+
+        case DISPLAY_ALERT:
+            return {
+                alert: action.payload
+            }
+        case HIDE_ALERT:
+            return {
+                alert: null
+            }
+
+        default:
+            return state;
+    }
+}
