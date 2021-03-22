b5744ddf3428f4aff2d099a6af48ab47a63a5daf
vidal-Rosc <vidal.rocca86@gmail.com>
2021-03-22 10:52:04 +0100

    app.js

--- FrontEnd/client/src/App.js
+++ FrontEnd/client/src/App.js
@@ -6,22 +6,27 @@ import Projects from './components/projects/Projects';
 import ProjectState from './context/projects/ProjectState';
 import TaskState from './context/tasks/taskState';
 import AlertState  from './context/alerts/alertState';
+import AuthState from './context/authentication/authState';
 
 
 function App() {
   //Todo lo que va dentro el Switch es el contenido de CADA UNA de las paginas
   //Todo lo que va fuera del switch se vera EN TODAS las paginas
+
+  console.log(process.env.REACT_APP_BACKEND_URL)
   return (
     <ProjectState>
       <TaskState>
         <AlertState>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Login} />
-              <Route exact path="/new-account" component={NewAccount} />
-              <Route exact path="/projects" component={Projects} />
-            </Switch>
-          </Router>
+          <AuthState>
+            <Router>
+              <Switch>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/new-account" component={NewAccount} />
+                <Route exact path="/projects" component={Projects} />
+              </Switch>
+            </Router>
+          </AuthState>
         </AlertState>
         </TaskState>
     </ProjectState>
