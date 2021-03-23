813cd9c2fed4bb2163fc9ff83b03ca12491098c3
vidal-Rosc <vidal.rocca86@gmail.com>
2021-03-23 12:18:38 +0100

    Agregando un nuevo type

--- FrontEnd/client/src/types/index.js
+++ FrontEnd/client/src/types/index.js
@@ -4,12 +4,13 @@ export const ADD_PROJECT = 'ADD_PROJECT';
 export const VALIDATE_FORM = 'VALIDATE_FORM';
 export const ACTUAL_PROJECT = 'ACTUAL_PROJECT';
 export const DELETE_PROJECT = 'DELETE_PROJECT';
+export const ERROR_PROJECT = 'ERROR_PROJECT';
+
 
 export const TASKS_PROJECT = 'TASKS_PROJECT';
 export const ADD_TASK = 'ADD_TASK';
 export const VALIDATE_TASK = 'VALIDATE_TASK';
 export const DELETE_TASK = 'DELETE_TASK';
-export const TASK_STATUS = 'TASK_STATUS';
 export const ACTUAL_TASK = 'ACTUAL_TASK';
 export const UPDATE_TASK = 'UPDATE_TASK';
 export const CLEAN_TASK = 'CLEAN_TASK';
