4b4af5c9c37214cbb312e81be99fb486f4c181a2
vidal-Rosc <vidal.rocca86@gmail.com>
2021-03-23 12:26:30 +0100

    agregando las alertas

--- FrontEnd/client/src/components/projects/ProjectList.js
+++ FrontEnd/client/src/components/projects/ProjectList.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import Project from '../projects/Project';
 import ProjectContext from '../../context/projects/ProjectContext';
+import AlertContext from '../../context/alerts/alertContext';
 import { CSSTransition, TransitionGroup } from  'react-transition-group';
 
 
@@ -9,13 +10,19 @@ const ProjectList = () => {
     const projectsContext = useContext(ProjectContext);
 
     //requerimos solo los proyectos, por ende solo extraemos los proyectos
-    const { proyectos, getProjects } = projectsContext;
+    const { proyectos, getProjects, message } = projectsContext;
+
+    const alertContext = useContext(AlertContext);
+    const {alert, displayAndHideAlert } = alertContext;
 
     //Para obtener los proyectos cuando carga el componente
     useEffect(() => {
+        //Si hay un error
+        if(message) displayAndHideAlert(message.msg, message.category);
+
         getProjects();
         //eslint-disable-next-line
-    },[]);
+    },[message]);
     
     //Revisamos si hay contenido en proyectos
     if(proyectos.length === 0) return <p>There's not Projects yet. Let's start one :)</p>;
@@ -23,10 +30,11 @@ const ProjectList = () => {
 
     return ( 
         <ul className="list-projects">
+            {alert ? ( <div className={`alert ${alert.category}`}>{alert.msg}</div>) : null}
           <TransitionGroup>
                 {proyectos.map(project => (
                    <CSSTransition
-                        key={project.id}
+                        key={project._id}
                         timeout={300}
                         classNames="project"
                    >
@@ -36,7 +44,6 @@ const ProjectList = () => {
                    </CSSTransition>
                 ))} 
           </TransitionGroup>
-
         </ul>
     );
 }
